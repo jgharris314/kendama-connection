e36bca356e33e56fec5b43e34439f867c393ad03
Jacob <jgharris314@gmail.com>
2024-02-14 17:07:55 -0700

    first commit

---
+++ frontend/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image"
+
+export default function Home() {
+  return (
+    <main>
+      <div className="flex w-full h-[10rem] items-center justify-center bg-blue-500">
+        Kendama Connetion
+      </div>
+    </main>
+  )
+}
